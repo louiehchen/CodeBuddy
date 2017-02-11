87353247e7ade3e0cb95bcbb077baeba7133d376
Louie <louiehchen@gmail.com>
2017-02-11 15:20:26 -0800

    remove counter, start for real

--- src/main.js
+++ src/main.js
@@ -2,11 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Counter from './Counter';
 import config from '../config';
+var gmap_key = 'AIzaSyAyesbQMyKVVbBgKVi2g6VX7mop2z96jBo';
 
-document.addEventListener('DOMContentLoaded', function() {
-  ReactDOM.render(
-    React.createElement(Counter),
-    document.getElementById('mount')
-  );
-});
+
+
 
