76c9eeb820a558f2625a90dacefcd6edaba71d1f
Louie <louiehchen@gmail.com>
2017-02-11 15:12:08 -0800

    test api

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Counter from './Counter';
- 
+import config from '../config';
+
 document.addEventListener('DOMContentLoaded', function() {
   ReactDOM.render(
     React.createElement(Counter),
